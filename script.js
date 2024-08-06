70a7ddf2f7b0282b8b191fd4b1ebf02598749ff1
CatDevil <2975527435@qq.com>
2024-08-06 12:01:51 +0800

    trying

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const raw = JSON.stringify({
             "query": query,
-            "lang": lang ? lang : en
+            "lang": lang ? lang : "en"
         });
 
         const requestOptions = {
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
             redirect: "follow"
         };
 
-        fetch("https://server.zeabur.internal:3000/api/tools/query", requestOptions)
+        fetch("https://server.zeabur.internal:8080/api/tools/query", requestOptions)
             .then((response) => response.json())
             .then((result) => {
                 if(result.error === undefined) {
